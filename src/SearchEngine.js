ea9ee1531e280fdd8edf976b134cb528d56f3203
Blessedbrita <blessedmasunika@gmail.com>
2024-03-13 23:15:15 +0200

    styles edit

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import axios from "axios";
-import styles from "./styles.css";
 
 export default function SearchEngine() {
     let [city, setCity] = useState(" ");
